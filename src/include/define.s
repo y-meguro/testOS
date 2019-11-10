d8985cde27817cf567d0fa59288859fc67407f27
Yohei Meguro <y.meguro.2008@gmail.com>
2019-11-11 08:43:52 +0900

    add 09_read_chs

---
+++ src/include/define.s
@@ -0,0 +1,5 @@
+BOOT_LOAD equ 0x7C00                    ; ブートプログラムのロード位置
+
+BOOT_SIZE equ (1024 * 8)                ; ブートコードサイズ
+SECT_SIZE equ (512)                     ; セクタサイズ
+BOOT_SECT equ (BOOT_SIZE / SECT_SIZE)   ; ブートプログラムのセクタ数
--- src/include/macro.s
+++ src/include/macro.s
@@ -1,13 +1,20 @@
 %macro cdecl 1-*.nolist
-  %rep %0 - 1
-    push %{-1:-1}
+    %rep %0 - 1
+        push %{-1:-1}
+        %rotate -1
+    %endrep
     %rotate -1
-  %endrep
-  %rotate -1
 
-  call %1
+    call %1
 
-  %if 1 < %0
-    add sp, (__BITS__ >> 3) * (%0 - 1)
-  %endif
+    %if 1 < %0
+        add sp, (__BITS__ >> 3) * (%0 - 1)
+    %endif
 %endmacro
+
+struc drive
+    .no resw 1      ; ドライブ番号
+    .cyln resw 1    ; シリンダ
+    .head resw 1    ; ヘッド
+    .sect resw 1    ; セクタ
+endstruc
