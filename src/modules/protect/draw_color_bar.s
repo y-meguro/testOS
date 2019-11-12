fa88f8e33884ec88c57a9f096d31ada03154f950
Yohei Meguro <y.meguro.2008@gmail.com>
2019-11-12 22:32:43 +0900

    add 21_draw_color_bar

--- src/modules/protect/draw_color_bar.s
+++ src/modules/protect/draw_color_bar.s
@@ -1,4 +1,4 @@
-draw_str:
+draw_color_bar:
         ;-----------------------------------------
         ; 【スタックフレームの構築】
         ;-----------------------------------------
@@ -13,38 +13,45 @@ draw_str:
         push	ecx
         push	edx
         push	esi
+        push	edi
 
         ;-----------------------------------------
-        ; 文字列を描画する
+        ; 基準となる位置をレジスタに保存
         ;-----------------------------------------
-        mov ecx, [ebp + 8]
-        mov edx, [ebp + 12]
-        movzx ebx, word [ebp + 16]
-        mov esi, [ebp + 20]
+        mov esi, [ebp + 8]
+        mov edi, [ebp + 12]
 
-        cld
+        ;-----------------------------------------
+        ; カラーバーを表示
+        ;-----------------------------------------
+        mov ecx, 0
 .10L:
-        lodsb
-        cmp al, 0
-        je .10E
+        cmp ecx, 16
+        jae .10E
 
-        cdecl draw_char, ecx, edx, ebx, eax
+        mov eax, ecx
+        and eax, 0x01
+        shl eax, 3
+        add eax, esi
+
+        mov ebx, ecx
+        shr ebx, 1
+        add ebx, edi
+
+        mov edx, ecx
+        shl edx, 1
+        mov edx, [.t0 + edx]
+
+        cdecl draw_str, eax, ebx, edx, .s0
 
         inc ecx
-        cmp ecx, 80
-        jl .12E
-        mov ecx, 0
-        inc edx
-        cmp edx, 30
-        jl .12E
-        mov edx, 0
-.12E:
         jmp .10L
 .10E:
 
         ;-----------------------------------------
         ; 【レジスタの復帰】
         ;-----------------------------------------
+        pop edi
         pop esi
         pop edx
         pop ecx
@@ -58,3 +65,13 @@ draw_str:
         pop ebp
 
         ret
+
+.s0:    db '        ', 0
+.t0:    dw 0x0000, 0x0800
+        dw 0x0100, 0x0900
+        dw 0x0200, 0x0A00
+        dw 0x0300, 0x0B00
+        dw 0x0400, 0x0C00
+        dw 0x0500, 0x0D00
+        dw 0x0600, 0x0E00
+        dw 0x0700, 0x0F00
