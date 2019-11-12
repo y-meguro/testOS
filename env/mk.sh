ee1dd61d7f7b8bd7588a92af37850a549cb72c01
Yohei Meguro <y.meguro.2008@gmail.com>
2019-11-12 15:54:15 +0900

    add 15_load_kernel

--- env/mk.sh
+++ env/mk.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-nasm boot.s -o boot.img -l boot.lst
+if [ -e kernel.s ]; then
+  nasm boot.s -o boot.bin -l boot.lst
+  nasm kernel.s -o kernel.bin -l kernel.lst
+  cat boot.bin kernel.bin > boot.img
+else
+  nasm boot.s -o boot.img -l boot.lst
+fi
--- src/include/define.s
+++ src/include/define.s
@@ -1,7 +1,14 @@
-BOOT_LOAD equ 0x7C00                    ; ブートプログラムのロード位置
+BOOT_SIZE equ (1024 * 8)
+KERNEL_SIZE equ (1024 * 8)
 
-BOOT_SIZE equ (1024 * 8)                ; ブートコードサイズ
-SECT_SIZE equ (512)                     ; セクタサイズ
-BOOT_SECT equ (BOOT_SIZE / SECT_SIZE)   ; ブートプログラムのセクタ数
+BOOT_LOAD equ 0x7C00
+BOOT_END equ (BOOT_LOAD + BOOT_SIZE)
+
+KERNEL_LOAD equ 0x0010_1000
+
+SECT_SIZE equ (512)
+
+BOOT_SECT equ (BOOT_SIZE / SECT_SIZE)
+KERNEL_SECT equ (KERNEL_SIZE / SECT_SIZE)
 
 E820_RECORD_SIZE equ 20
