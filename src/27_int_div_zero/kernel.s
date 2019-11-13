dee95e46fadf2c440e507772ce67d9cfa4ba4bd0
Yohei Meguro <y.meguro.2008@gmail.com>
2019-11-13 16:12:57 +0900

    add 27_int_div_zero

---
+++ src/27_int_div_zero/kernel.s
@@ -0,0 +1,77 @@
+;***********************************************************
+;  マクロ
+;***********************************************************
+%include "../include/define.s"
+%include "../include/macro.s"
+
+        ORG KERNEL_LOAD
+
+[BITS 32]
+;***********************************************************
+;  エントリポイント
+;***********************************************************
+kernel:
+        ;------------------------------------------
+        ; フォントアドレスを取得
+        ;------------------------------------------
+        mov esi, BOOT_LOAD + SECT_SIZE
+        movzx eax, word[esi + 0]
+        movzx ebx, word[esi + 2]
+        shl eax, 4
+        add eax, ebx
+        mov [FONT_ADR], eax
+
+        ;------------------------------------------
+        ; 割り込みベクタの初期化
+        ;------------------------------------------
+        cdecl init_int
+        set_vect 0x00, int_zero_div
+
+        ;------------------------------------------
+        ; フォントの一覧表示
+        ;------------------------------------------
+        cdecl draw_font, 63, 13
+        cdecl draw_color_bar, 63, 4
+
+        ;------------------------------------------
+        ; 文字列を描画
+        ;------------------------------------------
+        cdecl draw_str, 25, 14, 0x010F, .s0
+
+        ;------------------------------------------
+        ; 0除算による割り込みを生成
+        ;------------------------------------------
+        mov al, 0
+        div al
+
+        ;------------------------------------------
+        ; 処理の終了
+        ;------------------------------------------
+        jmp $
+
+.s0:    db " Hello, kernel! ", 0
+
+ALIGN 4, db 0
+FONT_ADR: dd 0
+RTC_TIME: dd 0
+
+;***********************************************************
+;  モジュール
+;***********************************************************
+%include "../modules/protect/vga.s"
+%include "../modules/protect/draw_char.s"
+%include "../modules/protect/draw_font.s"
+%include "../modules/protect/draw_str.s"
+%include "../modules/protect/draw_color_bar.s"
+%include "../modules/protect/draw_pixel.s"
+%include "../modules/protect/draw_line.s"
+%include "../modules/protect/draw_rect.s"
+%include "../modules/protect/itoa.s"
+%include "../modules/protect/rtc.s"
+%include "../modules/protect/draw_time.s"
+%include "modules/interrupt.s"
+
+;***********************************************************
+;  パディング
+;***********************************************************
+        times KERNEL_SIZE - ($ - $$) db 0
--- src/include/macro.s
+++ src/include/macro.s
@@ -12,6 +12,21 @@
     %endif
 %endmacro
 
+%macro  set_vect 1-*
+    push eax
+    push edi
+
+    mov edi, VECT_BASE + (%1 * 8)
+    mov eax, %2
+
+    mov [edi + 0], ax
+    shr eax, 16
+    mov [edi + 6], ax
+
+    pop edi
+    pop eax
+%endmacro
+
 struc drive
     .no resw 1      ; ドライブ番号
     .cyln resw 1    ; シリンダ
