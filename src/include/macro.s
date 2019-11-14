12c63be48c17d45da0a174682a79ebfdd60f8c81
Yohei Meguro <y.meguro.2008@gmail.com>
2019-11-14 17:08:10 +0900

    add 31, 32, and 33

--- src/include/macro.s
+++ src/include/macro.s
@@ -12,7 +12,7 @@
     %endif
 %endmacro
 
-%macro  set_vect 1-*
+%macro set_vect 1-*.nolist
         push eax
         push edi
 
@@ -31,9 +31,29 @@
         pop eax
 %endmacro
 
-%macro  outp 2
+%macro outp 2
     mov al, %2
     out %1, al
+%endmacro
+
+%macro set_desc 2-*
+        push eax
+        push edi
+
+        mov edi, %1
+        mov eax, %2
+
+    %if 3 == %0
+        mov [edi + 0], %3
+    %endif
+
+        mov [edi + 2], ax
+        shr eax, 16
+        mov [edi + 4], al
+        mov [edi + 7], ah
+
+        pop edi
+        pop eax
 %endmacro
 
 struc drive
